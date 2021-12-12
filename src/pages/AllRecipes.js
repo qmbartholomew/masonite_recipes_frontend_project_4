5307e9f99da4a7b41a392e48078857db63e5dc6b
Quentin <malcomb7@yahoo.com>
2021-12-11 16:41:18 -0800

    More styling - show page updates

--- src/pages/AllRecipes.js
+++ src/pages/AllRecipes.js
@@ -2,9 +2,11 @@ import Recipe from "../components/recipe"
 
 
 const AllRecipes = (props) => {
-    return props.recipes.map((r) => {
+    return <div className='iDisplay'>
+        {props.recipes.map((r) => {
         return <Recipe key={r.id} recipe={r} />
-    })
+        })}
+    </div>
 }
 
 export default AllRecipes
